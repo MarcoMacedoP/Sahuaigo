66a1b966bfa6c67604d4cfab732a95caeba77584
Marco <marcosiegman01@gmail.com>
2020-07-27 23:25:21 -0500

    Component Hotel working on mobile

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,8 @@
 import { AppPropsType } from "next/dist/next-server/lib/utils";
 import "reset-css/reset.css";
 import "../styles/global.sass";
+import "pure-react-carousel/dist/react-carousel.es.css";
 
 export default function App({ Component, pageProps }: AppPropsType) {
-  return <Component {...pageProps} />;
+	return <Component {...pageProps} />;
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,40 @@
 import Head from "next/head";
-
+import styles from "./index.module.sass";
 import { ContentWithImage } from "../components/ContentWithImage";
+import { Hotel } from "../components/Hotel";
+import { GetStaticProps } from "next";
+
+interface HomeProps {}
 
 export default function Home() {
-  return (
-    <main>
-      <ContentWithImage
-        imageUrl="/assets/img/landing-1.jpg"
-        title="Experimenta Sahuayo."
-        content="En Sahuayo se encuentra lo mejor de Michoacan. Atreveté a  conocerlo y reserva con nosotros."
-        isPageInitial
-      />
-      <ContentWithImage
-        imageUrl="/assets/img/landing-2.jpg"
-        title="Lol"
-        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam id maximus est. Aliquam arcu nunc, blandit a massa vel, ultricies ultricies dui. Proin in accumsan orci. Aliquam posuere mauris a tristique viverra. Cras sodales risus diam, non commodo purus rutrum sed. Mauris"
-        isInverted
-      />
-    </main>
-  );
+	return (
+		<main>
+			<ContentWithImage
+				imageUrl="/assets/img/landing-1.jpg"
+				title="Experimenta Sahuayo."
+				content="En Sahuayo se encuentra lo mejor de Michoacan. Atreveté a  conocerlo y reserva con nosotros."
+				isPageInitial
+			/>
+			<ContentWithImage
+				imageUrl="/assets/img/landing-2.jpg"
+				title="Lol"
+				content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam id maximus est. Aliquam arcu nunc, blandit a massa vel, ultricies ultricies dui. Proin in accumsan orci. Aliquam posuere mauris a tristique viverra. Cras sodales risus diam, non commodo purus rutrum sed. Mauris"
+				isInverted
+			/>
+			<article className={styles.hotelsContainer}>
+				<h3 className={styles.subtitle}>
+					Conoce nuestros mejores hoteles
+				</h3>
+				<Hotel title="Nombre del hotel" description={`
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam id maximus est. Aliquam arcu nunc, blandit a massa vel, ultricies ultricies dui. Proin in accumsan orci. Aliquam posuere mauris a tristique viverra. Cras sodales risus diam, non commodo purus rutrum sed. Mauris 
+`} />
+			</article>
+		</main>
+	);
 }
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+	return {
+		props: {}
+	};
+};
