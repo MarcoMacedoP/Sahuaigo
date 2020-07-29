cfade65ba4dbea4196d94c19e7635be0aa874ec9
Marco <marcosiegman01@gmail.com>
2020-07-29 17:11:07 -0500

    component hotel responsive

--- src/components/Hotel/index.tsx
+++ src/components/Hotel/index.tsx
@@ -2,104 +2,77 @@ import React from "react";
 import styles from "./component.module.sass";
 import { Button } from "../Button";
 import {
-	CarouselProvider,
-	Slider,
-	Slide,
-	ButtonBack,
-	ButtonNext
+  CarouselProvider,
+  Slider,
+  Slide,
+  ButtonBack,
+  ButtonNext
 } from "pure-react-carousel";
 
 interface HotelProps {
-	desc: string;
-	title: string;
+  desc: string;
+  title: string;
+  isInverted?: boolean;
 }
 
 export const Hotel = (props: HotelProps) => {
-	const slides = [1, 2];
-	const [currentIndex, setCurrentIndex] = React.useState(0);
-	const onNextSlide = () => setCurrentIndex(currentIndex + 1);
+  const slides = [1, 2];
+  const [currentIndex, setCurrentIndex] = React.useState(0);
+  const onNextSlide = () => setCurrentIndex(currentIndex + 1);
 
-	const onLastSlide = () => setCurrentIndex(currentIndex - 1);
+  const onLastSlide = () => setCurrentIndex(currentIndex - 1);
 
-	return (
-		<div className={styles.container}>
-			<section className={styles.section}>
-				<p className={styles.title}>
-					<strong> {props.title} </strong>{" "}
-				</p>
-				<CarouselProvider
-					naturalSlideWidth={100}
-					naturalSlideHeight={100}
-					totalSlides={2}
-				>
-					<Slider className={styles.slider}>
-						{slides.map((slide, index) => (
-							<Slide
-								index={index}
-								key={slide}
-							>
-								<div
-									className={
-										styles.slide
-									}
-								>
-									<img
-										className={
-											styles.hotelImage
-										}
-										src="https://pix10.agoda.net/hotelImages/124/1246280/1246280_16061017110043391702.jpg?s=1024x768"
-									/>
-								</div>
-							</Slide>
-						))}
-					</Slider>
-					<section
-						className={styles.slideControls}
-					>
-						<ButtonBack
-							onClick={onLastSlide}
-						>
-							<img
-								className={
-									styles.arrowBack
-								}
-								src="/assets/icons/arrow_forward.svg"
-							/>
-						</ButtonBack>
-						<ButtonNext
-							onClick={onNextSlide}
-						>
-							<img src="/assets/icons/arrow_forward.svg" />
-						</ButtonNext>
-						<div
-							className={
-								styles.slideDots
-							}
-						>
-							{slides.map(
-								(
-									slide,
-									index
-								) => (
-									<div
-										className={`${
-											styles.slideDot
-										} ${index ===
-											currentIndex &&
-											styles.slideDotSelected} `}
-									/>
-								)
-							)}
-						</div>
-					</section>
-				</CarouselProvider>
-			</section>
-			<section className={styles.section}>
-				<p className={styles.description}>
-					{props.description}
-				</p>
-				<Button text="Reserva en este hotel" />
-			</section>
-		</div>
-	);
+  return (
+    <div
+      className={props.isInverted ? styles.containerInverted : styles.container}
+    >
+      <section className={styles.section}>
+        <p className={styles.title}>
+          <strong> {props.title} </strong>
+        </p>
+        <CarouselProvider
+          naturalSlideWidth={100}
+          naturalSlideHeight={100}
+          totalSlides={2}
+        >
+          <Slider className={styles.slider}>
+            {slides.map((slide, index) => (
+              <Slide index={index} key={slide}>
+                <div className={styles.slide}>
+                  <img
+                    className={styles.hotelImage}
+                    src="https://pix10.agoda.net/hotelImages/124/1246280/1246280_16061017110043391702.jpg?s=1024x768"
+                  />
+                </div>
+              </Slide>
+            ))}
+          </Slider>
+          <section className={styles.slideControls}>
+            <ButtonBack onClick={onLastSlide}>
+              <img
+                className={styles.arrowBack}
+                src="/assets/icons/arrow_forward.svg"
+              />
+            </ButtonBack>
+            <ButtonNext onClick={onNextSlide}>
+              <img src="/assets/icons/arrow_forward.svg" />
+            </ButtonNext>
+            <div className={styles.slideDots}>
+              {slides.map((slide, index) => (
+                <div
+                  className={`${styles.slideDot} ${index === currentIndex &&
+                    styles.slideDotSelected} `}
+                />
+              ))}
+            </div>
+          </section>
+        </CarouselProvider>
+      </section>
+      <section className={styles.section}>
+        <p className={styles.titleDesktop}> {props.title} </p>
+        <p className={styles.description}>{props.description}</p>
+        <Button text="Reserva en este hotel" />
+      </section>
+    </div>
+  );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,34 +7,36 @@ import { GetStaticProps } from "next";
 interface HomeProps {}
 
 export default function Home() {
-	return (
-		<main>
-			<ContentWithImage
-				imageUrl="/assets/img/landing-1.jpg"
-				title="Experimenta Sahuayo."
-				content="En Sahuayo se encuentra lo mejor de Michoacan. Atreveté a  conocerlo y reserva con nosotros."
-				isPageInitial
-			/>
-			<ContentWithImage
-				imageUrl="/assets/img/landing-2.jpg"
-				title="Lol"
-				content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam id maximus est. Aliquam arcu nunc, blandit a massa vel, ultricies ultricies dui. Proin in accumsan orci. Aliquam posuere mauris a tristique viverra. Cras sodales risus diam, non commodo purus rutrum sed. Mauris"
-				isInverted
-			/>
-			<article className={styles.hotelsContainer}>
-				<h3 className={styles.subtitle}>
-					Conoce nuestros mejores hoteles
-				</h3>
-				<Hotel title="Nombre del hotel" description={`
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam id maximus est. Aliquam arcu nunc, blandit a massa vel, ultricies ultricies dui. Proin in accumsan orci. Aliquam posuere mauris a tristique viverra. Cras sodales risus diam, non commodo purus rutrum sed. Mauris 
-`} />
-			</article>
-		</main>
-	);
+  return (
+    <main>
+      <ContentWithImage
+        imageUrl="/assets/img/landing-1.jpg"
+        title="Experimenta Sahuayo."
+        content="En Sahuayo se encuentra lo mejor de Michoacan. Atreveté a  conocerlo y reserva con nosotros."
+        isPageInitial
+      />
+      <ContentWithImage
+        imageUrl="/assets/img/landing-2.jpg"
+        title="Lol"
+        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam id maximus est. Aliquam arcu nunc, blandit a massa vel, ultricies ultricies dui. Proin in accumsan orci. Aliquam posuere mauris a tristique viverra. Cras sodales risus diam, non commodo purus rutrum sed. Mauris"
+        isInverted
+      />
+      <article className={styles.hotelsContainer}>
+        <h3 className={styles.subtitle}>Conoce nuestros mejores hoteles</h3>
+        {[1, 2, 3, 4, 5, 6].map((hotel, index) => (
+          <Hotel
+            isInverted={index % 2 !== 0}
+            title="Nombre del hotel"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam id maximus est. Aliquam arcu nunc, blandit a massa vel, ultricies ultricies dui. Proin in accumsan orci. Aliquam posuere mauris a tristique viverra. Cras sodales risus diam, non commodo purus rutrum sed. Mauris"
+          />
+        ))}
+      </article>
+    </main>
+  );
 }
 
 export const getStaticProps: GetStaticProps<HomeProps> = async () => {
-	return {
-		props: {}
-	};
+  return {
+    props: {}
+  };
 };
