0db6d377ebab39328640ab2c5e56e189aaf10d89
Marco <marcosiegman01@gmail.com>
2020-08-04 13:43:36 -0500

    Data readed from json files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,54 +8,38 @@ import { Input } from "../components/Input";
 import { LandingForm } from "../components/LandingForm";
 import { Service } from "../components/Service";
 
-interface HomeProps {}
+interface Information {
+  title: string;
+  content: string;
+  imageUrl: string;
+}
+interface HomeProps {
+  information: Information[];
+  hotels: Hotel[];
+  services: {
+    title: string;
+    content: string;
+    data: Information[];
+  };
+}
 
-const hotels: Hotel[] = [
-  {
-    id: 1,
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam id maximus est. Aliquam arcu nunc, blandit a massa vel, ultricies ultricies dui. Proin in accumsan orci. Aliquam posuere mauris a tristique viverra. Cras sodales risus diam, non commodo purus rutrum sed. Mauris",
-    images: [],
-    name: "Nombre del hotel",
-  },
-  {
-    id: 2,
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam id maximus est. Aliquam arcu nunc, blandit a massa vel, ultricies ultricies dui. Proin in accumsan orci. Aliquam posuere mauris a tristique viverra. Cras sodales risus diam, non commodo purus rutrum sed. Mauris",
-    images: [],
-    name: "Nombre del hotel",
-  },
-  {
-    id: 3,
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam id maximus est. Aliquam arcu nunc, blandit a massa vel, ultricies ultricies dui. Proin in accumsan orci. Aliquam posuere mauris a tristique viverra. Cras sodales risus diam, non commodo purus rutrum sed. Mauris",
-    images: [],
-    name: "Nombre del hotel",
-  },
-  {
-    id: 4,
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam id maximus est. Aliquam arcu nunc, blandit a massa vel, ultricies ultricies dui. Proin in accumsan orci. Aliquam posuere mauris a tristique viverra. Cras sodales risus diam, non commodo purus rutrum sed. Mauris",
-    images: [],
-    name: "Nombre del hotel",
-  },
-];
-
-export default function Home() {
+export default function Home({
+  hotels,
+  information,
+  services,
+}: HomeProps) {
   return (
     <main>
-      <ContentWithImage
-        imageUrl="/assets/img/landing-1.jpg"
-        title="Experimenta Sahuayo."
-        content="En Sahuayo se encuentra lo mejor de Michoacan. Atreveté a  conocerlo y reserva con nosotros."
-        isPageInitial
-      />
-      <ContentWithImage
-        imageUrl="/assets/img/landing-2.jpg"
-        title="Lol"
-        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam id maximus est. Aliquam arcu nunc, blandit a massa vel, ultricies ultricies dui. Proin in accumsan orci. Aliquam posuere mauris a tristique viverra. Cras sodales risus diam, non commodo purus rutrum sed. Mauris"
-        isInverted
-      />
+      {information.map((element, index) => (
+        <ContentWithImage
+          key={index}
+          title={element.title}
+          content={element.content}
+          imageUrl={element.imageUrl}
+          isPageInitial={index === 0}
+        />
+      ))}
+
       <article className={styles.hotelsContainer}>
         <h3 className={styles.subtitle}>
           Conoce nuestros mejores hoteles
@@ -69,22 +53,20 @@ export default function Home() {
           />
         ))}
       </article>
+
       <article className={styles.services}>
-        <h4 className={styles.servicesTitle}>Nuestros servicios</h4>
+        <h4 className={styles.servicesTitle}>{services.title} </h4>
         <p className={styles.servicesDescription}>
-          Además de ofrecerte los mejores hoteles de Sahuayo tambien
-          contamos con los siguientes servicios
+          {services.content}
         </p>
-        <Service
-          description="Despreocupate en llegar a tu hotel. Nosotros te llevamos con todas las comodidades."
-          name="Transporte aeropuerto-hotel"
-          image="https://images.pexels.com/photos/1588073/pexels-photo-1588073.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-        />
-        <Service
-          description="Te mostramos lo mejor de Sahuayo."
-          name="Recorridos turisticos"
-          image="/assets/img/landing-2.jpg"
-        />
+        {services.data.map((service, index) => (
+          <Service
+            key={index}
+            name={service.title}
+            description={service.content}
+            image={service.imageUrl}
+          />
+        ))}
       </article>
       <LandingForm hotels={hotels} />
     </main>
@@ -92,7 +74,16 @@ export default function Home() {
 }
 
 export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const hotels = (await import("../db/Hotels.json")).default;
+  const information = (await import("../db/LandingInformation.json"))
+    .default;
+  const services = (await import("../db/Services.json")).default;
+
   return {
-    props: {},
+    props: {
+      hotels,
+      information,
+      services,
+    },
   };
 };
